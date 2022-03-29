7d45dc33e2c753afc70fcc477f3540fcda6e125e
pappurathod <prathod4545@gmail.com>
2022-03-29 15:19:54 +0530

    Create Binary Search Tree

---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to binary search tree");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            
+            binarySearchTree.Display();
+        }
+    }
+}
